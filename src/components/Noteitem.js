8754bbe05ad6973ef0c65a2397f8e8bdbd1578c9
Tanushree <tmm181092@gmail.com>
2023-06-21 20:45:05 +0530

    Adding "delete note" functionality

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
 
 const Noteitem = (props) => {
-  const { note } = props;
+
+    const context = useContext(NoteContext);
+    const { deleteNote } = context;
+  
+    const { note } = props;
+
   return (
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
             <div className="d-flex align-item-center">
                 <h5 className="card-title">{note.title}</h5>
-                <i className="bi bi-trash3 mx-2"></i>
+                <i className="bi bi-trash3 mx-2" onClick={()=>{deleteNote(note._id)}}></i>
                 <i className="bi bi-pencil-square mx-2"></i>
           </div>
           <p className="card-text">{note.description}</p>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -50,12 +50,15 @@ const NoteState = (props) => {
     }
 
     //   Delete a Note
-    const deleteNote = () => {
-        
+    const deleteNote = (id) => {
+        // TODO: API call
+        console.log("Deleting note with id", id)
+        const newNotes = notes.filter((note)=>{return note._id !== id})
+        setNotes(newNotes)
     }
 
     //   Edit a Note
-    const editNote = () => {
+    const editNote = (id, title, description, tag) => {
         
     }
     
