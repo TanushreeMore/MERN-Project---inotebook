b3a9a996e645c04d3db4932bfdd1ba27e61dcb9e
Tanushree <tmm181092@gmail.com>
2023-06-21 15:06:42 +0530

    use of useLocation hook

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,10 @@ import React, { useContext, useEffect } from "react";
 import NoteContext from "../context/notes/NoteContext";
 
 const About = () => {
-  const a = useContext(NoteContext);
-  
-  useEffect(() => {
-    a.update()
-    // eslint-disable-next-line
-  }, [])
   
   return (
     <>
       <h1>About</h1>
-      <h4>{a.state.name}. And she was in {a.state.class}.</h4>
     </>
   );
 };
