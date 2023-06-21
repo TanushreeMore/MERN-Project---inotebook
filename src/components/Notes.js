0c09e18dcf8c09d41105dba7ec19bc945b97a203
Tanushree <tmm181092@gmail.com>
2023-06-21 17:24:12 +0530

    Adding font awesome icons

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -9,7 +9,7 @@ const Notes = () => {
     <div className="row my-3">
       <h2>Your Notes</h2>
       {
-        notes.map((note, i)=>{ return <Noteitem key={i} note={note} /> ;})
+        notes.map((note)=>{ return <Noteitem key={note._id} note={note} /> ;})
       }
     </div>
   )
